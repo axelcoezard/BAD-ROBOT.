67691e8cc5f532c4819fc48ffdf75a6c5d5ed90a
Axel Coézard <axel.coezard@gmail.com>
2021-04-27 22:03:17 +0200

    Ajout de la commande !queue

--- src/commands.js
+++ src/commands.js
@@ -18,7 +18,7 @@ class Commands {
         this.playing = false;
         this.videoVolume = 50;
     }
-    
+
     /**
      * 
      */
@@ -59,53 +59,7 @@ class Commands {
      * 
      */
     async queue (message, args) {
-        const textChannel = message.channel;
-        if  (this.videoQueue.length > 0) {
-            const embed = new MessageEmbed()
-            .setColor("GREEN")
-            .setTitle(`Contenu de la file d'attente`)
-            .setDescription("Voici tous les titres qui sont dans la file d'attente:")
-            .setTimestamp();
-
-            this.videoQueue.forEach(async (videoURL, i) => {
-                const videoInfo = await ytdl.getBasicInfo(videoURL);
-                const videoName = await videoInfo.videoDetails.title;
-                const videoDuration = await videoInfo.videoDetails.length_seconds;
-                embed.addField(
-                    "\u200b",
-                    `**○** \`${videoName.substring(0, 250)}...\``,
-                );
-                if (i == this.videoQueue.length - 1) {
-                    embed.addField("\u200b", `**${this.videoQueue.length} titres dans la file d'attente**`, "");
-                    textChannel.send(embed)
-                }
-            })
-        } else {
-            textChannel.send(`:x: **La file d'attente est actuellement vide**...`);
-        }
-    }
-
-    /**
-     * 
-     */
-    async helpme (message, args) {
-        const embed = new MessageEmbed()
-        .setColor("GREEN") 
-        .setTitle("Liste des commandes")
-        .setDescription("Voici toutes les commandes du BAD ROBOT.:")
-        .addField("`!join`", "faire que le bot rejoigne le channel vocal.")
-        .addField("`!leave`", "faire que le bot à quitte le channel vocal.")
-        .addField("`!play`", "relance la lecture de la file d'attente.")
-        .addField("`!play` <lien_video_youtube>", "ajoute la musique correspondante à la liste d'attente et lance immediatement la lecture si elle est seule dedans.")
-        .addField("`!play` <lien_playlist_youtube>", "ajoute toutes les musiques de la playlist à la liste d'attente et lance immediatement la lecture si aucune musique n'est déjà entrain de se jouer.")
-        .addField("`!pause`", "met en pause la lecture de la musique.")
-        .addField("`!pause`", "met en pause la lecture de la musique.")
-        .addField("`!meme`", "renvoie un meme choisi aléatoirement depuis des subreddits prédéfinis.")
-        .addField( "**[NFSW]** `!pls` <nom_de_subbreddit>", "renvoie une image choisi aléatoirement depuis le subreddit indiqué.")
-        .setTimestamp();
-
-        message.channel.send(embed)
-        console.log("[BOT] Envoie de la liste des commandes")
+       
     }
 }
 
--- src/commands/queue.js
+++ src/commands/queue.js
@@ -1,5 +1,30 @@
 const Queue = async (client, message, args, state) => {
+    const textChannel = message.channel;
+    const {queue} = state.audio;
+
+    if  (queue.length == 0) {
+        return textChannel.send(`:x: **La file d'attente est actuellement vide**...`);
+    }
     
+    const embed = new MessageEmbed()
+    .setColor("GREEN")
+    .setTitle(`Contenu de la file d'attente`)
+    .setDescription("Voici tous les titres qui sont dans la file d'attente:")
+    .setTimestamp();
+
+    queue.forEach(async (videoURL, i) => {
+        const videoInfo = await ytdl.getBasicInfo(videoURL);
+        const videoName = await videoInfo.videoDetails.title;
+        const videoDuration = await videoInfo.videoDetails.length_seconds;
+        embed.addField(
+            "\u200b",
+            `**○** \`${videoName.substring(0, 250)}...\``,
+        );
+        if (i == queue.length - 1) {
+            embed.addField("\u200b", `**${queue.length} titres dans la file d'attente**`, "");
+            textChannel.send(embed)
+        }
+    });
 }
 
 export default Queue;
